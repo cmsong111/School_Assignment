beba4060cd63b34b83336c0e1cf875013165d136
Kimnamju <cmsong111@naver.com>
2022-03-18 01:07:04 +0900

    22.03.18 update

--- 컴퓨터 그래픽스/22.03.17.cpp
+++ 컴퓨터 그래픽스/22.03.17.cpp
@@ -1,17 +1,17 @@
-/*  날짜:
+/*  날짜: 22.03.17(목)
 	작성자: 김남주
-	주제: 콜백 관련 기본 소스	*/
+	주제: 콘솔 디버깅	*/
 
-#include<glut.h>
-#include<glu.h>
-#include<gl.h>
+#include <glut.h>
+#include <glu.h>
+#include <gl.h>
 #include <iostream>
-
 
 int now_x = 0, now_y = 0, now_mouse = 0;
 float R = 1.0F, G = 1.0F, B = 1.0F;
 
-void MyDisplay() {
+void MyDisplay()
+{
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(R, G, B);
 	glBegin(GL_POLYGON);
@@ -23,80 +23,89 @@ void MyDisplay() {
 	glFlush();
 }
 
-void MyResahpe(int NewWidth, int NewHeight) {
-
+void MyResahpe(int NewWidth, int NewHeight)
+{
 }
 
-void MyKeyBoard(unsigned char KeyPressed, int X, int Y) {
+void MyKeyBoard(unsigned char KeyPressed, int X, int Y)
+{
 	std::cout << "MyKeyBoard :" << KeyPressed << " " << X << " " << Y << std::endl;
 }
 
-void MySpecial(int key, int X, int Y) {
+void MySpecial(int key, int X, int Y)
+{
 	std::cout << key << " " << X << " " << Y << std::endl;
 }
 
-void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y) {
+void MyMouseClick(GLint Button, GLint State, GLint X, GLint Y)
+{
 	std::cout << "MyMouseClick :" << Button << " " << State << " " << X << " " << Y << std::endl;
 	now_x = X;
 	now_y = Y;
 	now_mouse = Button;
-	if (now_mouse == 0) {
+	if (now_mouse == 0 && State == 0)
+	{
 		R = 1.0F, G = 0.0F, B = 0.0F;
 	}
-	else if (now_mouse == 1) {
+	else if (now_mouse == 1 && State == 0)
+	{
 		R = 0.0F, G = 1.0F, B = 0.0F;
 	}
-	else if (now_mouse == 2) {
+	else if (now_mouse == 2 && State == 0)
+	{
 		R = 0.0F, G = 0.0F, B = 1.0F;
 	}
 
 	glutPostRedisplay();
-
 }
 
-void MyMouseMove(GLint X, GLint Y) {
-	//std::cout << "MyMouseMove:" << X << " " << Y << std::endl;
+void MyMouseMove(GLint X, GLint Y)
+{
+	// std::cout << "MyMouseMove:" << X << " " << Y << std::endl;
 
-	if (now_mouse == 0) {
+	if (now_mouse == 0)
+	{
 		std::cout << "좌클릭 하면서 ";
 	}
-	else if (now_mouse == 1) {
+	else if (now_mouse == 1)
+	{
 		std::cout << "휠클릭 하면서 ";
 	}
-	else if (now_mouse == 2) {
+	else if (now_mouse == 2)
+	{
 		std::cout << "우클릭 하면서 ";
 	}
 
-	if (now_x > X) {
+	if (now_x > X)
+	{
 		std::cout << "왼쪽으로 ";
 	}
-	else {
+	else
+	{
 		std::cout << "오른쪽으로 ";
 	}
 
-
 	std::cout << "이동중" << std::endl;
-
 }
 
-void MyIdle() {
-
+void MyIdle()
+{
 }
 
-void MyTimer(int Value) {
-
+void MyTimer(int Value)
+{
 }
 
-void MenuProc(int entryID) {
-
+void MenuProc(int entryID)
+{
 }
 
-void MenuFunc() {
-
+void MenuFunc()
+{
 }
 
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB);
 	glutInitWindowSize(500, 500);
@@ -106,14 +115,14 @@ int main(int argc, char** argv) {
 
 	glutDisplayFunc(MyDisplay);
 
-	//glutReshapeFunc(MyResahpe);
-	//glutKeyboardFunc(MyKeyBoard);
-	//glutSpecialFunc(MySpecial);
+	// glutReshapeFunc(MyResahpe);
+	glutKeyboardFunc(MyKeyBoard);
+	glutSpecialFunc(MySpecial);
 	glutMouseFunc(MyMouseClick);
 	glutMotionFunc(MyMouseMove);
-	//glutIdleFunc(MyIdle);
-	//glutTimerFunc(40, MyTimer, 1);
-	//MenuFunc();
+	// glutIdleFunc(MyIdle);
+	// glutTimerFunc(40, MyTimer, 1);
+	// MenuFunc();
 
 	glutMainLoop();
 	return 0;
